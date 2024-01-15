b6fc5930ab07aa6c83a838a8dd7db4c1299a7acf
spicon <spicon@travelcompositor.com>
2024-01-15 18:58:51 +0100

    Cambio el nombre del test

--- src/test/java/com/trc/massage/SetupTest.java
+++ src/test/java/com/trc/massage/SetupTest.java
@@ -1,17 +1,12 @@
 package com.trc.massage;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import java.time.LocalDate;
 
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.greaterThan;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
-public class MassageGatewayIT {
+public class SetupTest {
 
     @Test
     public void setup() {
